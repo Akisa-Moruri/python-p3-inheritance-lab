bc2faacb3d890e87283f9a8135e721735414fcbf
Akisa-Moruri <akisamoruri@gmail.com>
2024-05-27 22:00:47 +0300

    inheritance lab1

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,14 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [""]
+
     def teach(self):
-        pass
+        if not self.knowledge:
+            return "I have no knowledge to teach."
+        else:
+            return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
+
+    
